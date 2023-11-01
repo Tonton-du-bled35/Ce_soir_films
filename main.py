542876fff39f24ea00a16d1a817eae7cdcccc96a
Tonton-du-bled35 <103746652+Tonton-du-bled35@users.noreply.github.com>
2023-11-01 03:57:40 +0100

    toto

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-toto = 1
+import requests
+
+url = 'https://api.themoviedb.org/3/movie/11'
+params = {
+    'api_key': '0600b7eb309eab604656515827689e1b'
+}
+
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    movie_data = response.json()
+    # Utilisez movie_data pour traiter les données du film
+    print(movie_data)
+else:
+    print(f"La requête a échoué avec le code d'état {response.status_code}")
