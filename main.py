62609a5ac5da5aca24ef663f53a4520589fccdc3
Tonton-du-bled35 <103746652+Tonton-du-bled35@users.noreply.github.com>
2023-11-23 14:47:24 +0100

    modification du html et css

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ response = requests.get(url, params=params)
 if response.status_code == 200:
     movie_data = response.json()
     # Utilisez movie_data pour traiter les données du film
-    print(movie_data)
+    # print(movie_data)
 else:
     print(f"La requête a échoué avec le code d'état {response.status_code}")
+
+print(movie_data["poster_path"])
+
+image=movie_data["poster_path"]
